af3223e97ab33caf35c0cb548e1c5039336b1454
athilkabbas123 <a.kuzhikkandathil@accenture.com>
2023-04-17 12:28:25 +0100

    added authorization for inteli api
    Signed-off-by: athilkabbas123 <a.kuzhikkandathil@accenture.com>

--- inteli-api/index.js
+++ inteli-api/index.js
@@ -2,10 +2,24 @@ const axios = require('axios')
 const methods = {}
 require('dotenv').config()
 methods.sendNotification = async (data) => {
+    const authData = {
+        "client_id": process.env.AUTH_CLIENT_ID,
+        "client_secret": process.env.AUTH_SECRET,
+        "audience": process.env.AUTH_AUDIENCE,
+        "grant_type": process.env.AUTH_GRANT_TYPE
+    }
+    const urlAuth = 'https://inteli-poc.eu.auth0.com/oauth/token'
+    let response = await axios(urlAuth,{
+        method: 'POST',
+        data: authData
+    })
+    let headers = {}
+    headers['Authorization'] = `Bearer ${response.access_token}`
     const url = `http://${process.env.INTELI_API_HOST}:${process.env.INTELI_API_PORT}/v1/notification`
     return axios(url, {
         method: 'POST',
-        data: data
+        data: data,
+        headers: headers
         }
     )
 }

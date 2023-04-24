204c0d59a568fa3f3febb2248a3183f558f8b443
athilkabbas123 <a.kuzhikkandathil@accenture.com>
2023-04-24 15:55:57 +0100

    adding all types of notification and authentication
    Signed-off-by: athilkabbas123 <a.kuzhikkandathil@accenture.com>

--- inteli-api/index.js
+++ inteli-api/index.js
@@ -2,10 +2,14 @@ const axios = require('axios')
 const methods = {}
 require('dotenv').config()
 methods.sendNotification = async (data) => {
+    const headers = {
+        'Authorization': `Bearer ${process.env.AUTH_TOKEN}`
+    }
     const url = `http://${process.env.INTELI_API_HOST}:${process.env.INTELI_API_PORT}/v1/notification`
     return axios(url, {
         method: 'POST',
-        data: data
+        data: data,
+        headers: headers
         }
     )
 }
